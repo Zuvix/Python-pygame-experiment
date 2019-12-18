2ad570fa83fa824674d0affa0410785b3be888a1
sweil9 <samuelkubala@gmail.com>
2019-12-18 13:31:10 +0100

    Art Asstes

--- firstGame.py
+++ firstGame.py
@@ -1,14 +1,25 @@
 import pygame
 
+# Init Section
 pygame.init()
 win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("First Game")
 
-vel = 5
+
+class Player:
+    def __init__(self, x, y, width, height, velocity):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.velocity = velocity
+
+
+player = Player(50, 50, 50, 50, 5)
 run = True
-x = 50
-y = 50
 clock = pygame.time.Clock()
+
+# Gameloop
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -16,15 +27,15 @@ while run:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        x -= vel
+        player.x -= player.velocity
     if keys[pygame.K_RIGHT]:
-        x += vel
+        player.x += player.velocity
     if keys[pygame.K_DOWN]:
-        y += vel
+        player.y += player.velocity
     if keys[pygame.K_UP]:
-        y -= vel
+        player.y -= player.velocity
     win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, 40, 60))
+    pygame.draw.rect(win, (255, 0, 0), (player.x, player.y, 40, 60))
     pygame.display.update()
     win.fill((0))
     clock.tick(60)
