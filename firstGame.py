49602942615c2f60285bb9062e25b5c77a3e2f0f
sweil9 <samuelkubala@gmail.com>
2019-12-22 01:28:46 +0100

    fixed bullet
    + Added sounds

--- firstGame.py
+++ firstGame.py
@@ -6,10 +6,9 @@ win_height = 700
 win_width = 600
 win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("First Game")
-player_image = pygame.image.load('assets\images\Ship.png')
 
 
-class projectile(object):
+class Projectile(object):
     def __init__(self, x, y, color, vel, is_enemy):
         self.x = x
         self.y = y
@@ -17,7 +16,14 @@ class projectile(object):
         self.vel = vel
         self.is_enemy = is_enemy
 
-class circle_projectile(projectile):
+    def draw(self):
+        pass
+
+    def move(self):
+        pass
+
+
+class CircleProjectile(Projectile):
     def __init__(self, radius, *args):
         self.radius = radius
         super().__init__(*args)
@@ -25,68 +31,116 @@ class circle_projectile(projectile):
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
-class rect_projectile(projectile):
+
+class RectProjectile(Projectile):
     def __init__(self, width, height, *args):
-        self.radius = width
-        self.height =height
+        self.width = width
+        self.height = height
         super().__init__(*args)
 
+    def draw(self, win):
+        pygame.draw.rect(win, self.color,
+                         (self.x, self.y, self.width, self.height))
+
+
 class Player:
-    def __init__(self, x, y, width, height, velocity, max_cooldown):
+    player_image = pygame.image.load('assets\images\Ship.png')
+
+    def __init__(self, x, y, width, height, vel, can_fire):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.velocity = velocity
-        self.max_cooldown = max_cooldown
-        self.cooldown = 0
+        self.vel = vel
+        self.can_fire = can_fire
 
     def draw(self, win):
-        win.blit(player_image, (player.x, player.y))
+        win.blit(self.player_image, (self.x, self.y))
 
 
-player = Player(win_width // 2 - 50, win_height - 32, 60, 32, 5, 30)
-run = True
-clock = pygame.time.Clock()
+class Enemy:
+    def __init__(self, x, y, width, height, vel, life):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = vel
+        self.life = life
+
+    def draw(self, win):
+        move()
+        win.blit(self.enemy_image, (self.x, self.y))
+
+    def move(self, win):
+        if self.y < 500:
+            self.y += self.vel
+
+    def shoot(self):
+        pass
 
 
-def redrawGameWindow():
+class AlphaEnemy(Enemy):
+    enemy_image1 = pygame.image.load('assets\images\InvaderA1.png')
+    enemy_image2 = pygame.image.load('assets\images\InvaderA2.png')
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+def redraw_game_window():
     win.fill((0, 0, 0))
     player.draw(win)
     for bullet in bullets:
         bullet.draw(win)
+    if player_bullet != None:
+        player_bullet.draw(win)
     pygame.display.update()
 
 
-def handlePlayerInput():
+def handle_player_input():
+    global player_bullet
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and player.x > 0:
-        player.x -= player.velocity
+        player.x -= player.vel
     if keys[pygame.K_RIGHT] and player.x < win_width - player.width:
-        player.x += player.velocity
-    if keys[pygame.K_SPACE] and player.cooldown == 0:
-        bullets.append(circle_projectile(6,round(player.x + player.width // 2),
-            round(player.y), (0, 200, 0), 5, True))
-        print("penis")
-        print("x ", bullets[0].x, "y ", bullets[0].y)
-        print(bullets[0].color)
+        player.x += player.vel
+    if keys[pygame.K_SPACE] and player.can_fire:
+        player.can_fire = False
+        player_bullet = RectProjectile(6, 20,
+                                       round(player.x + player.width // 2 - 3),
+                                       round(player.y), (0, 200, 0), 6, True)
+
+
+'''        bullets.append(
+            CircleProjectile(5, round(player.x + player.width // 2),
+                             round(player.y), (0, 200, 0), 6, True))
         player.cooldown = player.max_cooldown
     if player.cooldown > 0:
         player.cooldown -= 1
+'''
 
-
+player = Player(win_width // 2 - 50, win_height - 32, 60, 32, 5, True)
+run = True
+clock = pygame.time.Clock()
 bullets = []
+player_bullet = None
 # Gameloop
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    handlePlayerInput()
+    handle_player_input()
+    if (player_bullet != None):
+        if (player_bullet.y > -5):
+            player_bullet.y -= player_bullet.vel
+        else:
+            player_bullet = None
+            player.can_fire = True
     for bullet in bullets:
-        if bullet.y > -5:
-            bullet.y -= bullet.vel
+        if bullet.y < win_height:
+            bullet.y += bullet.vel
         else:
             bullets.pop(bullets.index(bullet))
-    redrawGameWindow()
+    redraw_game_window()
     clock.tick(60)
 pygame.quit()
