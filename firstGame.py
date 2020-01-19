deb32bb6d3b1f566b4d90c03d55154b7325e4956
sweil9 <samuelkubala@gmail.com>
2020-01-19 03:09:09 +0100

    Score system

--- firstGame.py
+++ firstGame.py
@@ -9,8 +9,8 @@ pygame.init()
 win_height = 600
 win_width = 800
 win = pygame.display.set_mode((win_width, win_height))
+font = pygame.font.Font(os.path.join("assets", "fonts", 'unifont.ttf'), 16)
 pygame.display.set_caption("Space Invaders by Zuvix")
-
 #Load Sound Assets
 bullet_sound = pygame.mixer.Sound(
     os.path.join('assets', 'nSounds', 'shoot.wav'))
@@ -33,6 +33,7 @@ YELLOW = pygame.Color(241, 255, 0)
 BLUE = pygame.Color(0, 150, 240)
 PURPLE = pygame.Color(203, 0, 255)
 RED = pygame.Color(237, 28, 36)
+WHITE = pygame.Color(255, 255, 255)
 
 
 def calculate_velocity(number_of_enemies: int):
@@ -89,7 +90,7 @@ class RectProjectile(Projectile):
 
 class Player:
     player_image = img_p
-    player_image = pygame.transform.scale(player_image, (45, 24))
+    player_image = pygame.transform.scale(player_image, (42, 21))
 
     def __init__(self, x, y, width, height, vel):
         self.x = x
@@ -114,11 +115,11 @@ class Enemy:
         self.counter = 0 + anim_offset
         self.image_id = 1
         i1 = image_1
-        i1 = pygame.transform.scale(i1, (32, 24))
+        i1 = pygame.transform.scale(i1, (28, 21))
         set_color(i1, color)
         self.enemy_image1 = i1
         i2 = image_2
-        i2 = pygame.transform.scale(i2, (32, 24))
+        i2 = pygame.transform.scale(i2, (28, 21))
         set_color(i2, color)
         self.enemy_image2 = i2
         self.enemy_image = i1
@@ -152,6 +153,19 @@ class Enemy:
         print("destroyed", self.score)
 
 
+class Text:
+    def __init__(self, font, color, x, y):
+
+        self.font = font
+        self.color = color
+        self.x = x
+        self.y = y
+
+    def draw(self, window, text):
+        text_obj = self.font.render(text, 1, self.color)
+        window.blit(text_obj, (self.x, self.y))
+
+
 def redraw_game_window():
     win.fill((0, 0, 0))
     player.draw(win)
@@ -162,6 +176,7 @@ def redraw_game_window():
     anim_speed = calculate_animation_speed(len(enemies))
     for enemy in enemies:
         enemy.draw(win, anim_speed)
+    score_text.draw(win, "Score: " + str(score))
     pygame.display.update()
 
 
@@ -188,17 +203,17 @@ def spawn_enemies():
     for d in range(1):
         for i in range(11):
             enemies.append(
-                Enemy(x + i * 48, y + d * 38 + 8, 32, 24, 30, d * 15, ORANGE,
+                Enemy(x + i * 44, y + d * 32 + 8, 32, 24, 30, d * 15, ORANGE,
                       img_c1, img_c2))
     for d in range(1, 3):
         for i in range(11):
             enemies.append(
-                Enemy(x + i * 48, y + d * 38 + 8, 32, 24, 20, d * 15, PURPLE,
+                Enemy(x + i * 44, y + d * 32 + 8, 32, 24, 20, d * 15, PURPLE,
                       img_b1, img_b2))
     for d in range(3, 5):
         for i in range(11):
             enemies.append(
-                Enemy(x + i * 48, y + d * 38 + 8, 32, 24, 10, d * 15,
+                Enemy(x + i * 44, y + d * 32 + 8, 32, 24, 10, d * 15,
                       pygame.Color(0, 100, 255), img_a1, img_a2))
     return enemies
 
@@ -210,6 +225,8 @@ clock = pygame.time.Clock()
 bullets = []
 player_bullet = None
 direction = "right"
+score = 0
+score_text = Text(font, WHITE, 10, 10)
 
 # Gameloop
 enemies = spawn_enemies()
@@ -231,7 +248,6 @@ while run:
         else:
             bullets.pop(bullets.index(bullet))
 
-    enemy: Enemy
     check_wall = False
     for enemy in enemies:
         if direction == "right" and enemy.x + enemy.width >= win_width:
@@ -251,6 +267,7 @@ while run:
                                            enemy.width, enemy.height)):
                 player_bullet = None
                 player.can_fire = True
+                score += enemy.score
                 enemy.destroy()
                 enemy_death_sound.play()
                 enemies.pop(enemies.index(enemy))
